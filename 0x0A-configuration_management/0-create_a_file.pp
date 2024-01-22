c25cb789c9d75a9caee61d1c15634d9c4fd93443
deantosh <deantoshdaiddoh@gmail.com>
2024-01-22 08:27:17 -0800

    task 0: create /tmp/school

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,17 +1,3 @@
-# Ensure group exists
-group{ 'www-data':
-  ensure  => present,
-}
-
-
-# Ensure user 'www-data' exists 
-user{ 'www-data':
-  ensure     => present,
-  managehome => true,
-  gid        => 'www-data',
-}
-
-
 # Creates `/tmp/school`
 file {'/tmp/school':
   ensure  => present,
