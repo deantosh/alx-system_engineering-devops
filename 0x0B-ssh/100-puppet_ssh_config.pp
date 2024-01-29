f29570a150121db8deb9920830410927bbc377f9
deantosh <deantoshdaiddoh@gmail.com>
2024-01-29 09:18:25 -0800

    Task 4: solution added

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,13 +6,13 @@ include stdlib
 # Update or Add 'PasswordAuthentication on'
 file_line { 'turn_off_password':
   ensure => present,
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '   PasswordAuthentication on',
 }
 
 # Update or add 'private key'
 file_line { 'Declare file identity':
   ensure => present,
-  path   => '~/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '   IdentityFile ~/.ssh/school',
 }
