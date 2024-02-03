602ec4435221d2b8d4bfc986e991216825beff0f
deantosh <deantoshdaiddoh@gmail.com>
2024-02-03 13:49:02 -0800

    Task 1: solution added

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  require => Package['python3-pip'],
 }
