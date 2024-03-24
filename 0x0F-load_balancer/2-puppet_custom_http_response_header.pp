d3838f2fa91c360c45a8387afeecd34302dbbe91
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-03-24 06:50:46 -0700

    task 2: solution updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,56 +1,19 @@
 # Puppet installs nginx server.
 # Configures it based on the requirements:
-#	- listen to port 80
-#	- add default response ("Hello World!")
-#	- add custom 404 error page (content = "Ceci n'est pas une page")
-#	- handle a single page redirection ("/redirect_me")
-#	- add custom header X-Served-By set to 'hostname'.
+#	- add custom header X-Served-By set to 'hostname'
 
-
-# install latest nginx version
-package {'nginx':
-  ensure => 'latest',
+# update apt-get
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-# config nginx -- add index page
-file {'/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# config nginx -- add custom 404 error page
-file {'/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page",
+-> file_line {'add_custom_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By ${hostname}",
 }
-
-# config nginx -- single page redirection (redirect_me)
-$line = "server_name _;\\n\\trewrite ^\\/redirect_me https:\\/\\/mattfarley.ca permanent;"
-
-exec {'redirect_me':
-  command  => inline_template("sed -i 's/server_name _;/${line}/' /etc/nginx/sites-enabled/default"),
-  provider => 'shell',
-}
-
-# config nginx -- redirect to custom 404 error page
-$str = "listen 80 default_server;\\n\\terror_page 404 \\/404.html;\\n\\t\
-location = \\/404.html{\\n\\t\\troot \\/var\\/www\\/html;\\n\\t\\tinternal;\\n\\t}"
-
-exec {'redirect_404_page':
-  command  => inline_template("sed -i 's/listen 80 default_server;/${str}/' /etc/nginx/sites-enabled/default"),
-  provider => 'shell',
-}
-
-# config nginx -- add custom X-Served-By header
-$new_line = "server_name _;\\n\\tadd_header X-Served-By ${hostname};"
-
-exec {'add_custom_header':
-  command  => "sed -i 's/server_name _;/${new_line}/' /etc/nginx/sites-enabled/default",
-  provider => 'shell',
-}
-
-# start nginx to update changes
-exec {'run':
-  command  => 'sudo service nginx start',
-  provider => 'shell',
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
