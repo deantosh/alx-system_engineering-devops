9991e539f8f9da0de2f579954a5ea6850d49c7fa
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-04-01 17:45:02 +0300

    task 0: solution updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
 """
-a function that returns the number of subscribers for a given subreddit
+Script queries the Reddit API and returns the number of subscribers\
+ (not active users, total subscribers) for a given subreddit. If an\
+ invalid subreddit is given, the function should return 0.
+
+Requirements:
+  - Prototype: def number_of_subscribers(subreddit)
+  - If not a valid subreddit, return 0.
+
+NOTE:
+  - Invalid subreddits may return a redirect to search results.
+  - Ensure that you are not following redirects.
 """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    number of not active users, total subscribers
-    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    # Set a custom User-Agent to avoid rate limiting
+    headers = {'User-Agent': 'windows:subs:v1.0.0 (by /u/deantosh)'}
 
     try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
+        res = requests.get(url, headers=headers)
+        res_dict = res.json()
+        if 'data' in res_dict and 'subscribers' in res_dict["data"]:
+            num_subs = res_dict["data"]["subscribers"]
+            return num_subs
         else:
             return 0
-    except Exception as e:
+    except Exception:
         return 0
