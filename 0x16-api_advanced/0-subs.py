b89a434bf4161c1752cf21cc37996c472c05d586
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-04-01 02:50:39 -0700

    task 0: solution added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+a function that returns the number of subscribers for a given subreddit
 """
-
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    number of not active users, total subscribers
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    # Set a custom User-Agent to avoid rate limiting
 
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
         return 0
+
