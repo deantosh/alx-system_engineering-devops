7fdb983a4b817d95eeae232fe253f101aebedbb9
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-08-18 11:11:48 +0000

    directory deleted

--- 0x17-web_stack__debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Manifest fixes apache2 internal server error:500
-file_line { 'fix_class_wp_locale_extension':
-  path  => '/var/www/html/wp-settings.php',
-  line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
-  match => '^require_once\( ABSPATH \. WPINC \. \'/class-wp-locale.phpp\' \);',
-}
